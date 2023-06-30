7f65de2fcf89a672ae9edf16c095709379cf1740
ibrahimahmedoglu <ibrahim.ahmedoglu@bahcesehir.edu.tr>
2023-06-30 13:44:34 +0300

    completed assigntment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    return Math.abs(blocks - 42);
+  }
+  
+  function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * 264;
+  }
+  
+  function distanceTravelledInFeet(startBlock, endBlock) {
+    const blocksTravelled = Math.abs(endBlock - startBlock);
+    return blocksTravelled * 264;
+  }
+  
+  function calculatesFarePrice(startBlock, endBlock) {
+    const distance = distanceTravelledInFeet(startBlock, endBlock);
+  
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
+  
